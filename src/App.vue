1c9b46b9c9e556d37d97d8cffeef2eaa704034d5
Criszrf <AlishiaCrieghtonlrh50@gmail.com>
2024-05-22 11:19:03 +0800

    主题定制

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
+import HelloWorld from '@/components/HelloWorld.vue';
 </script>
 
 <template>
   <RouterView />
+  <el-button type="primary">Default</el-button>
 </template>
 
 <style scoped>
