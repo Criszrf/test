2cbc7ef48d5f10aebfe07956019c35af8094d90c
Criszrf <AlishiaCrieghtonlrh50@gmail.com>
2024-05-25 10:53:10 +0800

    banner

--- src/views/Home/components/HomePanel.vue
+++ src/views/Home/components/HomePanel.vue
@@ -1,15 +1,4 @@
 <script setup>
-// 定义props
-defineProps({
-  // 主标题
-  title: {
-    type: String
-  },
-  // 副标题
-  subTitle: {
-    type: String
-  }
-})
 
 </script>
 
@@ -18,13 +7,13 @@ defineProps({
   <div class="home-panel">
     <div class="container">
       <div class="head">
-        <!-- 主标题和副标题 -->
+         <!-- 主标题和副标题 -->
         <h3>
-          {{ title }}<small>{{ subTitle }}</small>
+          新鲜好物<small>新鲜出炉 品质靠谱</small>
         </h3>
       </div>
       <!-- 主体内容区域 -->
-      <slot />
+      <div> 主体内容 </div>
     </div>
   </div>
 </template>
